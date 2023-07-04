84c13a2d01000736212b501fcbb696fdbc28b7c4
Dmytro <dimmmashevchenko@gmail.com>
2023-07-04 11:05:51 +0300

    add home work practice №3

--- script.js
+++ script.js
@@ -1,22 +1,227 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// ==============================Практика №1============================================================
 
+//const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+// const personalMovieDB = {
+//     count: numberOfFilms, 
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', ''),
+//         c = prompt('Один из последних просмотренных фильмов?', ''),
+//         d = prompt('На сколько оцените его?', '');
+      
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// =======================================================================================================
+
+ 
+//При помощи цикла выведите числа от 5 до 10 в консоль. 5 и 10 включительно. Цикл можно использовать любой
+// let i = 5;
+// while ( i <=10 ) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 5; 
+//     do {
+//         console.log(i);
+//         i++; 
+//     } while (i <=10);
+
+// for ( i = 0; i<=10; i++) {
+//     if (i<=4) continue;
+//     console.log(i);
+
+// }
+
+
+//При помощи цикла for вывести числа от 20 до 10 в консоль. В обратном порядке (20, 19, 18...). Когда цикл дойдет до числа 13 - остановить весь цикл
+// i = 20;
+// for (; i>10; i--) {
+//     if (i == 13) break;
+//     console.log(i);
+// }
+
+//При помощи цикла for выведите чётные числа от 2 до 10 включительно
+// for (i = 1; i <= 10; i++ ) {
+//     if (i % 2) continue ;
+//     console.log(i);
+// }
+
+
+// Перепишите цикл  for на вариант с while. Результат должен остаться точно таким же. Не создайте бесконечный цикл! Иначе браузер может зависнуть.
+
+//  Цикл, который нужно переписать:
+ 
+//  for (let i = 2; i <= 16; i++) {
+//      if (i % 2 === 0) {
+//          continue;
+//      } else {
+//          console.log(i);
+//      }
+//  }
+
+                            // i = 2;
+                            // while (i <= 16) {
+                            //     i++;
+                            //     if (i % 2 === 0) {
+                            //         continue;
+                            //     } 
+                            //         console.log(i);
+                            //     }
+    
+  
+// Заполните массив цифрами от 5 до 10 включительно. Помните, что элементы массива можно сформировать так же, как и обращаться к ним: arr[0]
+
+// const jon = [5, 6, 7, 8, 9, 10];
+// console.log(jon);
+
+// ===================================================================================================
+
+// Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд.
+// Должен получиться точно такой же массив
+
+// function getMathResult(num, times) {
+//     if (typeof(times) !== 'number' || times <= 0) {
+//         return num;
+//     }
+
+//     let str = '';
+
+//     for (let i = 1; i <= times; i++) {
+//         if (i === times) {
+//             str += `${num * i}`;
+//             // Тут без черточек в конце
+//         } else {
+//             str += `${num * i}---`;
+//             // Это тоже самое, что и
+//             // str = str + num * i + "---"
+//         }
+//     }
+
+//     return str;
+// }
+
+// getMathResult(10, 5);
+// ======================================================================================================
+                         //Методы и свойства строк и чисел
+
+// let str = 'dima';
+// //let arr = [1, 2, 3];
+
+// console.log(str.toLocaleUpperCase());
+// console.log(str.toLocaleUpperCase());
+// console.log(str);
+
+
+// const sport = 'Big boll';
+
+// console.log(sport.indexOf('c'));
+
+
+// const coll = "Hello Poltava";
+
+// console.log(coll.slice(-3, -1));
+
+
+// const num = '10.11px';
+
+// console.log(parseInt(num));
+// console.log(parseFloat(num));
+
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+'use strict';
+
+// Код возьмите из предыдущего домашнего задания
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
-    count: numberOfFilms, 
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', ''),
-        c = prompt('Один из последних просмотренных фильмов?', ''),
-        d = prompt('На сколько оцените его?', '');
-      
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
